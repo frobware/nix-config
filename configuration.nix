6a3fc4df3169e52508f136d4d2a10803735ddd90
Andrew McDermott <aim@frobware.com>
2019-04-10 21:46:02 +0100

    Initial t460s configuration
    My first play with NixOS - like what I see!

---
+++ configuration.nix
@@ -0,0 +1,153 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Use the GRUB 2 boot loader.
+  # boot.loader.grub.enable = true;
+  #boot.loader.grub.version = 2;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.earlyVconsoleSetup = true;
+
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  networking.hostName = "t460s";
+  # networking.wireless.enable = true;
+  networking.networkmanager.enable = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Sane font defaults
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fontconfig.cache32Bit = true;
+    fontconfig.ultimate.preset = "osx";
+
+    fonts = with pkgs; [
+      terminus_font
+      source-code-pro
+    ];
+  };
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont= "latarcyrheb-sun32";
+    #consoleKeyMap = "us";
+    consoleUseXkbConfig = true;
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget vim firefox emacs zsh bash bash-completion
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "ctrl:nocaps";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+  services.xserver.synaptics.enable = false;
+
+  services.xserver.config = ''
+    Section "InputClass"
+    Identifier     "Enable libinput for TrackPoint"
+    MatchIsPointer "on"
+    Driver         "libinput"
+    EndSection
+  '';
+
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
+  services.dbus.packages = with pkgs; [ gnome3.dconf ];
+  
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.guest = {
+  #   isNormalUser = true;
+  #   uid = 1000;
+  # };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+
+  programs.zsh.enable = true;
+
+  users.extraUsers = [
+  {
+    name = "aim";
+    uid = 1000;
+    group = "users";
+    extraGroups = [ "wheel" "networkmanager" "share" ];
+    description = "Andrew McDermott";
+    home = "/home/aim";
+    shell = "/run/current-system/sw/bin/zsh";
+  }
+  ];
+
+  users.extraGroups = [
+  { 
+    name = "share";
+    gid = 1001;
+  }
+  ];
+
+  hardware.trackpoint = {
+    enable = true;
+    sensitivity = 255;
+    speed = 255;
+    device = "TPPS/2 Elan TrackPoint";
+    #device = "PS/2 Generic Mouse";
+  };
+}
